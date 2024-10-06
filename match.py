7a8238cf4e2714b082c7bc3ae2cde7ee696ff3b2
Julián Lara <julian_lara143@hotmail.com>
2024-10-06 09:21:46 -0600

    ejemplo del uso de while

--- match.py
+++ match.py
@@ -15,18 +15,18 @@ def tax_by_level_person(level_person:str, money:int):
             return money * (1 + (normal_tax/100)) # money * 1 * .10
         case _:
             return  money * (1 + (30/100)) 
-        
-result = input("Escribe el nivel del cliente?")
-# todos los datos que ingresa el usuario la funcion input lo lee como str
-# es necesario hacer una conversion de dato ya que se van a hacer operaciones aritmeticas.
-money = int(input("coloca una cantidad"))
 
-# se hace uso de la funcion con los dos parametros anteriormente usado.
+
 repeat = True
-while(repeat):
+while(repeat):       
+    # se hace uso de la funcion con los dos parametros anteriormente usado.
+    result = input("Escribe el nivel del cliente? \n")
+    # todos los datos que ingresa el usuario la funcion input lo lee como str
+    # es necesario hacer una conversion de dato ya que se van a hacer operaciones aritmeticas.
+    money = int(input("coloca una cantidad \n"))
     show_print = tax_by_level_person(result, money)
     print(show_print)
-    option = input("Desea obtener otro impuesto? \n 1.- Si \n 2.- No")
+    option = input("Desea obtener otro impuesto? \n 1.- Si \n 2.- No \n")
     if(option.lower() =='2'.lower()):
         repeat = not repeat
     elif(option.lower() =='1'.lower()):
