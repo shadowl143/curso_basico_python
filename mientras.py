5310e37a1d3bb2b1f4f9393666062991be262a3d
JULIAN LARA <julian_lara143@hotmail.com>
2024-10-11 22:40:15 -0600

    ejercicio diamante

--- mientras.py
+++ mientras.py
@@ -26,5 +26,22 @@ def three():
     while(height_tronco>=0):
         print(' ' * 5 + caracter * 10 + ' ' * 5)
         height_tronco -= 1
+'''
+Creacion de un diamante.
+'''
+def diamon(heigth:int):
+    caracter = "*"
+    size = 1
+    # se genera la primer parte del diamante, la sintaxis permite omitir los parentesis.
+    while heigth > 0:
+        print(' ' * heigth + caracter * size + ' ' * heigth)
+        heigth -= 1
+        size+=2
+    # el tamaño que se va a regresar empieza a partir del valor maximo de tamaño.
+    while 0 < size:
+        print(' ' * heigth + caracter * size + ' ' * heigth)
+        heigth += 1
+        size -= 2
 
-three()
+# el tamaño del diamante es variable.
+diamon(2)
